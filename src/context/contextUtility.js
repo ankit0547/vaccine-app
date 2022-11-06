d631f4f9762fa8d994047affab9f7b7d617cb79c
Ankit sharma <ankit.sharma09547@gmail.com>
2022-11-06 21:31:33 +0530

    debugger removed

--- src/context/contextUtility.js
+++ src/context/contextUtility.js
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { appActionTypes, AppStore } from "./AppContext";
 
 export const DispatchLoader = (val) => {
-  debugger;
   const { appActionDispatch } = useContext(AppStore);
   appActionDispatch({
     type: appActionTypes.setLoader,
